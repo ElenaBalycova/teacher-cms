c6834247253b8c34dfd70079562ce1b1600ca93a
ElenaBalycova <elenavorob3@gmail.com>
2018-02-26 19:52:17 +0300

    Admin: category create

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -27,7 +27,11 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+      return view('admin.categories.create', [
+        'category'   => [],
+        'categories' => Category::with('children')->where('parent_id', '0')->get(),
+        'delimiter'  => ''
+      ]);
     }
 
     /**
@@ -36,10 +40,11 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
-    }
+     public function store(Request $request)
+     {
+          Category::create($request->all());
+          return redirect()->route('admin.category.index');
+     }
 
     /**
      * Display the specified resource.
